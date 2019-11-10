c4f76eeb00b7491023d9643f200f909bdcd5b3c8
Constantine Theocharis <kontheocharis@gmail.com>
2019-11-10 17:29:54 +0200

    Modify README.md, add screenshot

---
+++ README.md
@@ -0,0 +1,15 @@
+# Gravity Engine
+
+Built with p5.js and Typescript.
+
+### Installing & Usage
+
+```
+git clone https://github.com/Gaweph/p5-typescript-starter.git
+npm install
+npm start
+```
+
+Then open browser to `localhost:3000` and click to create planets.
+
+![Screenshot of the engine in action.](./img/screenshot.png "Screenshot of the engine in action.")
--- build/build.js
+++ build/build.js
@@ -1,109 +1,73 @@
-var Morph = (function () {
-    function Morph() {
+var Particle = (function () {
+    function Particle(radius, position, velocity) {
+        this.radius = radius;
+        this.position = position;
+        this.velocity = velocity;
+        this.acceleration = createVector(0, 0);
+        this.mass = 4 / 3 * PI * this.radius * this.radius * this.radius;
     }
-    Morph.prototype.setup = function () {
-        this.shapes = [];
-        this.currentShape = 0;
-        this.shapes.push({ points: Shapes.circle(100), color: color('#009CDF') });
-        this.shapes.push({ points: Shapes.circle(150), color: color(255, 204, 0) });
-        this.shapes.push({ points: Shapes.square(50), color: color(175, 100, 220) });
-        this.morph = new Array();
-        var highestCount = 0;
-        for (var i = 0; i < this.shapes.length; i++) {
-            highestCount = Math.max(highestCount, this.shapes[i].points.length);
-        }
-        for (var i = 0; i < highestCount; i++) {
-            this.morph.push(new p5.Vector());
-        }
-    };
-    Morph.prototype.recalc = function () {
-        var totalDistance = 0;
-        var points = this.shapes[this.currentShape].points;
-        for (var i = 0; i < points.length; i++) {
-            var v1 = points[i];
-            var v2 = this.morph[i];
-            v2.lerp(v1, 0.1);
-            totalDistance += p5.Vector.dist(v1, v2);
-        }
-        if (totalDistance < 0.1) {
-            this.currentShape++;
-            if (this.currentShape >= this.shapes.length) {
-                this.currentShape = 0;
-            }
-        }
-    };
-    Morph.prototype.draw = function () {
-        this.recalc();
-        var color = this.shapes[this.currentShape].color;
-        var points = this.shapes[this.currentShape].points;
-        translate(width / 2, height / 2);
-        strokeWeight(4);
-        beginShape();
-        noFill();
-        stroke(color);
-        for (var i = 0; i < points.length; i++) {
-            var v = this.morph[i];
-            vertex(v.x, v.y);
-        }
-        endShape(CLOSE);
-    };
-    return Morph;
+    return Particle;
 }());
-var Shapes = (function () {
-    function Shapes() {
-    }
-    Shapes.circle = function (size) {
-        var points = new Array();
-        for (var angle = 0; angle < 360; angle += 9) {
-            var v = p5.Vector.fromAngle(radians(angle - 135));
-            v.mult(size);
-            points.push(v);
-        }
-        return points;
-    };
-    Shapes.square = function (size) {
-        var points = new Array();
-        for (var x = -size; x < size; x += 10) {
-            points.push(createVector(x, -size));
-        }
-        for (var y = -size; y < size; y += 10) {
-            points.push(createVector(size, y));
-        }
-        for (var x = size; x > -size; x -= 10) {
-            points.push(createVector(x, size));
-        }
-        for (var y = size; y > -size; y -= 10) {
-            points.push(createVector(-size, y));
-        }
-        return points;
-    };
-    Shapes.star = function (x, y, radius1, radius2, npoints) {
-        var angle = TWO_PI / npoints;
-        var halfAngle = angle / 2.0;
-        var points = new Array();
-        for (var a = 0; a < TWO_PI; a += angle) {
-            var sx = x + cos(a) * radius2;
-            var sy = y + sin(a) * radius2;
-            points.push(createVector(sx, sy));
-            sx = x + cos(a + halfAngle) * radius1;
-            sy = y + sin(a + halfAngle) * radius1;
-            points.push(createVector(sx, sy));
-        }
-        return points;
-    };
-    return Shapes;
-}());
-var morph;
+var particles;
+var bigG = 20;
+var sunRadius = 75;
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    morph = new Morph();
-    morph.setup();
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    particles = [
+        new Particle(sunRadius, createVector(0, 0), createVector(0, 0)),
+    ];
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
+function unitDifference(a, b) {
+    var diff = p5.Vector.sub(b, a);
+    return p5.Vector.div(diff, p5.Vector.mag(diff));
+}
+function mousePressed() {
+    var posX = mouseX - windowWidth / 2;
+    var posY = mouseY - windowHeight / 2;
+    particles.push(new Particle(random(5, 15), createVector(posX, posY), p5.Vector.fromAngle(-atan2(posX, posY), sqrt(bigG * particles[0].mass / p5.Vector.dist(particles[0].position, createVector(posX, posY))))));
+}
 function draw() {
-    background(100);
-    morph.draw();
+    background(0);
+    fill(225, 200, 30);
+    for (var _i = 0, particles_1 = particles; _i < particles_1.length; _i++) {
+        var particle = particles_1[_i];
+        circle(particle.position.x, particle.position.y, particle.radius);
+    }
+    calculatePhysics();
+}
+function calculatePhysics() {
+    if (particles.length > 1) {
+        var _loop_1 = function (particle) {
+            var lastAcceleration = particle.acceleration;
+            particle.position.add(p5.Vector.add(p5.Vector.mult(particle.velocity, deltaTime / 1000), p5.Vector.mult(lastAcceleration, 0.5 * sq(deltaTime / 1000))));
+            particle.acceleration = particles
+                .filter(function (p) { return p != particle; })
+                .map(function (p) { return p5.Vector.mult(unitDifference(particle.position, p.position), p.mass * bigG / (sq(p5.Vector.dist(p.position, particle.position)))); })
+                .reduce(function (a, b) { return p5.Vector.add(a, b); });
+            var avgAcceleration = p5.Vector.add(lastAcceleration, particle.acceleration).div(2);
+            particle.velocity.add(p5.Vector.mult(avgAcceleration, deltaTime / 1000));
+        };
+        for (var _i = 0, particles_2 = particles; _i < particles_2.length; _i++) {
+            var particle = particles_2[_i];
+            _loop_1(particle);
+        }
+    }
+}
+function wrapParticle(particle) {
+    if (particle.position.x > windowWidth / 2) {
+        particle.position.x = particle.position.x - windowWidth;
+    }
+    else if (particle.position.x < -windowWidth / 2) {
+        particle.position.x = particle.position.x + windowWidth;
+    }
+    if (particle.position.y > windowHeight / 2) {
+        particle.position.y = particle.position.y - windowHeight;
+    }
+    else if (particle.position.y < -windowHeight / 2) {
+        particle.position.y = particle.position.y + windowHeight;
+    }
 }
 //# sourceMappingURL=build.js.map
